9eb85a4f48b0575f3b3be43786d9244d50603dff
Smelky <46086711+Smelky@users.noreply.github.com>
2019-03-08 01:58:38 +0200

    Addbranch

--- src/com/smelk/calculator/Calculator.java
+++ src/com/smelk/calculator/Calculator.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
-//
-//    public void calculate(double firstValue, double secondValue, char operation) {
-//        List<Double> values = new ArrayList<Double>();
-//        values.add(firstValue);
-//        values.add(secondValue);
-//        String function = String.valueOf(operation);
-//        try {
-//            System.out.println(calculator(values, function));
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//    }
-//
-//    private double calculator(List<Double> values, String function) {
-//        Operations result = Arrays.stream(Operations.values())
-//                .filter(operations -> operations.getOperator().equals(function))
-//                .findFirst()
-//                .orElseThrow();
-//        return result.getResult(values.get(0), values.get(1));
-//
-//    }
+
+    public void calculate(double firstValue, double secondValue, char operation) {
+        List<Double> values = new ArrayList<Double>();
+        values.add(firstValue);
+        values.add(secondValue);
+        String function = String.valueOf(operation);
+        try {
+            System.out.println(calculator(values, function));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private double calculator(List<Double> values, String function) {
+        Operations result = Arrays.stream(Operations.values())
+                .filter(operations -> operations.getOperator().equals(function))
+                .findFirst()
+                .orElseThrow();
+        return result.getResult(values.get(0), values.get(1));
+
+    }
 }
