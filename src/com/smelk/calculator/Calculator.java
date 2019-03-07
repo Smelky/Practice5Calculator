9e650e8a3d05e2829edcb3900dfb0732cd44cd48
Smelky <46086711+Smelky@users.noreply.github.com>
2019-03-08 01:55:49 +0200

    Initial commit

---
+++ src/com/smelk/calculator/Calculator.java
@@ -0,0 +1,30 @@
+package com.smelk.calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculator {
+//
+//    public void calculate(double firstValue, double secondValue, char operation) {
+//        List<Double> values = new ArrayList<Double>();
+//        values.add(firstValue);
+//        values.add(secondValue);
+//        String function = String.valueOf(operation);
+//        try {
+//            System.out.println(calculator(values, function));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//    }
+//
+//    private double calculator(List<Double> values, String function) {
+//        Operations result = Arrays.stream(Operations.values())
+//                .filter(operations -> operations.getOperator().equals(function))
+//                .findFirst()
+//                .orElseThrow();
+//        return result.getResult(values.get(0), values.get(1));
+//
+//    }
+}
---
+++ src/com/smelk/calculator/Operations.java
@@ -0,0 +1,30 @@
+package com.smelk.calculator;
+
+import java.util.function.DoubleBinaryOperator;
+
+public enum Operations {
+//
+//    ADDITION("+", (firstValue, secondValue) -> firstValue + secondValue),
+//    SUBTRACTION("-", (firstValue, secondValue) -> firstValue - secondValue),
+//    MULTIPLICATION("*", (firstValue, secondValue) -> firstValue * secondValue),
+//    DIVIDE("/", (firstValue, secondValue) -> firstValue / secondValue),
+//    ROOT("~", (firstValue, secondValue) -> Math.pow(firstValue, 1 / secondValue)),
+//    EXPONENTIATION("^", Math::pow),
+//    SPECIAL("$", (firstValue, secondValue) -> Math.pow((firstValue + secondValue) / firstValue + 117, secondValue));
+//
+//    private final String operator;
+//    private final DoubleBinaryOperator result;
+//
+//    Operations(String operator, DoubleBinaryOperator result) {
+//        this.operator = operator;
+//        this.result = result;
+//    }
+//
+//    public String getOperator() {
+//        return operator;
+//    }
+//
+//    public double getResult(double firstValue, double secondValue) {
+//        return result.applyAsDouble(firstValue, secondValue);
+//    }
+}
