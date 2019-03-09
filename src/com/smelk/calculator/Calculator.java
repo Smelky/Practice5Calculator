8910b7552d383f4cee93ec8c6b9429298dce5ff4
Smelky <46086711+Smelky@users.noreply.github.com>
2019-03-10 01:29:27 +0200

    Addbranch

--- src/com/smelk/calculator/Calculator.java
+++ src/com/smelk/calculator/Calculator.java
@@ -5,16 +5,10 @@ import java.util.Arrays;
 public class Calculator {
 
     public static double calculate(double firstValue, double secondValue, char operation) {
-        return calculateTwoValues(firstValue, secondValue, operation);
-
-    }
-
-    private static double calculateTwoValues(double firstValue, double secondValue, char function) {
         Operations result = Arrays.stream(Operations.values())
-                .filter(operations -> operations.getOperator().equals(function))
+                .filter(operations -> operations.getOperator().equals(operation))
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
         return result.getResult(firstValue, secondValue);
-
     }
 }
